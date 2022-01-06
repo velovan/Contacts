53637a6f442dd97037c12c9778fd184e74d1f88b
velovan <velovan@users.noreply.github.com>
2022-01-06 14:52:23 +0200

    Update FirstNameComparator.java

--- bookapp/src/sourceBook/FirstNameComparator.java
+++ bookapp/src/sourceBook/FirstNameComparator.java
@@ -6,6 +6,7 @@ public class FirstNameComparator implements Comparator<Contact> {
 
     @Override
     public int compare(Contact a, Contact b) {
+     
         return a.getFirstName().compareTo(b.getFirstName());
     }
 
